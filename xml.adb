c707bac3f07b4db2c71e61f8c2fe86bd4152334a
Alexander Senier <senier@componolit.com>
2018-05-26 23:50:12 +0200

    Support attributes

--- xml.adb
+++ xml.adb
@@ -5,16 +5,32 @@ package body XML
    with SPARK_Mode
 is
 
-   type Kind_Type is (Kind_Invalid, Kind_Element_Open, Kind_Element_Close);
+   type Kind_Type is (Kind_Invalid,
+                      Kind_Element_Open,
+                      Kind_Element_Close,
+                      Kind_Attr_Integer,
+                      Kind_Attr_Float,
+                      Kind_Attr_String);
 
    type Name_Type is new String (1..100);
+   Null_Name : constant Name_Type := (others => Character'Val (0));
 
-   type Node_Type is
+   type Node_Type (Kind : Kind_Type := Kind_Invalid) is
    record
-      Kind : Kind_Type;
       Name : Name_Type;
+      case Kind is
+         when Kind_Invalid
+            | Kind_Element_Open
+            | Kind_Element_Close => null;
+         when Kind_Attr_Integer =>
+            Integer_Value : Integer;
+         when Kind_Attr_Float =>
+            Float_Value : Float;
+         when Kind_Attr_String =>
+            String_Value : Name_Type;
+      end case;
    end record;
-   Null_Node : constant Node_Type := (Kind => Kind_Invalid, Name => (others => Character'Val (0)));
+   Null_Node : constant Node_Type := (Kind => Kind_Invalid, Name => Null_Name);
 
    type Subtree_Type is array (Natural range <>) of Node_Type;
    Null_Tree : constant Subtree_Type (1..0) := (others => Null_Node);
@@ -51,8 +67,8 @@ is
       return String (Name (1 .. Len));
    end To_String;
 
-   function E (Name     : String;
-               Children : Subtree_Type := Null_Tree) return Subtree_Type
+   function E (Name       : String;
+               Children   : Subtree_Type := Null_Tree) return Subtree_Type
    is
       Index : Natural;
    begin
@@ -69,21 +85,65 @@ is
       end return;
    end E;
 
+   function A (Name  : String;
+               Value : Integer) return Subtree_Type
+   is
+   begin
+      return (1 => (Kind          => Kind_Attr_Integer,
+                    Name          => To_Name (Name),
+                    Integer_Value => Value));
+   end A;
+
+   function A (Name  : String;
+               Value : Float) return Subtree_Type
+   is
+   begin
+      return (1 => (Kind        => Kind_Attr_Float,
+                    Name        => To_Name (Name),
+                    Float_Value => Value));
+   end A;
+
+   function A (Name  : String;
+               Value : String) return Subtree_Type
+   is
+   begin
+      return (1 => (Kind         => Kind_Attr_String,
+                    Name         => To_Name (Name),
+                    String_Value => To_Name (Value)));
+   end A;
+
    procedure Print (Tree : Subtree_Type)
    is
+      Is_Open : Boolean := False;
    begin
       for I in Tree'Range
       loop
          case Tree (I).Kind
          is
             when Kind_Element_Open =>
-               Put ("<" & To_String (Tree (I).Name) & ">");
+               if Is_Open
+               then
+                  Is_Open := False;
+                  Put (">");
+               end if;
+               Is_Open := True;
+               Put ("<" & To_String (Tree (I).Name));
             when Kind_Element_Close =>
+               if Is_Open
+               then
+                  Is_Open := False;
+                  Put (">");
+               end if;
                Put ("</" & To_String (Tree (I).Name) & ">");
+            when Kind_Attr_Integer =>
+               Put (" " & To_String (Tree (I).Name) & "=""" & Tree (I).Integer_Value'Img & """");
+            when Kind_Attr_String =>
+               Put (" " & To_String (Tree (I).Name) & "=""" & To_String (Tree (I).String_Value) & """");
+            when Kind_Attr_Float =>
+               Put (" " & To_String (Tree (I).Name) & "=""" & Tree (I).Float_Value'Img & """");
             when Kind_Invalid =>
                Put ("INVALID");
          end case;
-         New_Line;
       end loop;
    end Print;
 
@@ -92,10 +152,12 @@ is
    is
       Tree : Subtree_Type :=
        E ("foo",
+         A ("test", 1) &
+         A ("attr", 42) &
+         A ("strattr", "grml!") &
          E ("baz",
-           E ("blub")
-         ) &
-         E ("bar")
+           E ("blub", A ("blubattr", 124))) &
+         E ("bar", A ("float", 1.2533))
        );
    begin
       Print (Tree);
