e43bf22db2882ed7e7790d37d5e2b19c28de1f56
Alexander Senier <senier@componolit.com>
2018-08-12 13:59:37 +0200

    Add XML generator tests

--- tests/sxml_generator_tests.adb
+++ tests/sxml_generator_tests.adb
@@ -14,19 +14,48 @@ with SXML;
 
 package body SXML_Generator_Tests is
 
-   procedure Test_Generate_Node (T : in out Test_Cases.Test_Case'Class)
+   procedure Test_Generate_Single_Node (T : in out Test_Cases.Test_Case'Class)
    is
       use SXML;
+      Doc : Subtree_Type := E ("config");
+      XML : String := To_String (Doc);
    begin
-      Assert (False, "Not implemented");
-	end Test_Generate_Node;
+      Assert (XML = "<config/>",
+         "Unexpected document: " & XML & " len:" & XML'Length'Img);
+	end Test_Generate_Single_Node;
+
+   ---------------------------------------------------------------------------
+
+   procedure Test_Generate_Single_Node_Attrib (T : in out Test_Cases.Test_Case'Class)
+   is
+      use SXML;
+      Doc : Subtree_Type := E ("config", A ("attrib", "Foo"));
+      XML : String := To_String (Doc);
+   begin
+      Assert (XML = "<config attrib=""Foo""/>",
+         "Unexpected document: " & XML & " len:" & XML'Length'Img);
+	end Test_Generate_Single_Node_Attrib;
+
+   ---------------------------------------------------------------------------
+
+   procedure Test_Generate_Nodes (T : in out Test_Cases.Test_Case'Class)
+   is
+      use SXML;
+      Doc : Subtree_Type := E ("config", E ("child"));
+      XML : String := To_String (Doc);
+   begin
+      Assert (XML = "<config><child/></config>",
+         "Unexpected document: " & XML & " len:" & XML'Length'Img);
+	end Test_Generate_Nodes;
 
    ---------------------------------------------------------------------------
 
    procedure Register_Tests (T: in out Test_Case) is
       use AUnit.Test_Cases.Registration;
    begin
-      Register_Routine (T, Test_Generate_Node'Access, "Generate node");
+      Register_Routine (T, Test_Generate_Single_Node'Access, "Generate single node");
+      Register_Routine (T, Test_Generate_Single_Node_Attrib'Access, "Generate single node with attribute");
+      Register_Routine (T, Test_Generate_Nodes'Access, "Generate nodes");
    end Register_Tests;
 
    ---------------------------------------------------------------------------
