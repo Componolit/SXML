a0a5a5662b23d7072b79282b21154d0a44893897
Alexander Senier <senier@componolit.com>
2018-08-24 07:08:25 +0200

    Test String, Float and Integer attributes

--- tests/execute/sxml_generator_tests.adb
+++ tests/execute/sxml_generator_tests.adb
@@ -50,12 +50,51 @@ package body SXML_Generator_Tests is
 
    ---------------------------------------------------------------------------
 
+   procedure Test_String_Attribute (T : in out Test_Cases.Test_Case'Class)
+   is
+      use SXML;
+      Doc : Subtree_Type := E ("config", E ("child", A ("attr", "value")));
+      XML : String := To_String (Doc);
+   begin
+      Assert (XML = "<config><child attr=""value""></child></config>",
+         "Unexpected document: " & XML & " len:" & XML'Length'Img);
+	end Test_String_Attribute;
+
+   ---------------------------------------------------------------------------
+
+   procedure Test_Integer_Attribute (T : in out Test_Cases.Test_Case'Class)
+   is
+      use SXML;
+      Doc : Subtree_Type := E ("config", E ("child", A ("attr", 42)));
+      XML : String := To_String (Doc);
+   begin
+      Assert (XML = "<config><child attr=""42""></child></config>",
+         "Unexpected document: " & XML & " len:" & XML'Length'Img);
+	end Test_Integer_Attribute;
+
+   ---------------------------------------------------------------------------
+
+   procedure Test_Float_Attribute (T : in out Test_Cases.Test_Case'Class)
+   is
+      use SXML;
+      Doc : Subtree_Type := E ("config", E ("child", A ("attr", 3.14)));
+      XML : String := To_String (Doc);
+   begin
+      Assert (XML = "<config><child attr=""3.14000E+00""></child></config>",
+         "Unexpected document: " & XML & " len:" & XML'Length'Img);
+	end Test_Float_Attribute;
+
+   ---------------------------------------------------------------------------
+
    procedure Register_Tests (T: in out Test_Case) is
       use AUnit.Test_Cases.Registration;
    begin
       Register_Routine (T, Test_Generate_Single_Node'Access, "Generate single node");
       Register_Routine (T, Test_Generate_Single_Node_Attrib'Access, "Generate single node with attribute");
       Register_Routine (T, Test_Generate_Nodes'Access, "Generate nodes");
+      Register_Routine (T, Test_String_Attribute'Access, "String attribute");
+      Register_Routine (T, Test_Integer_Attribute'Access, "Integer attribute");
+      Register_Routine (T, Test_Float_Attribute'Access, "Float attribute");
    end Register_Tests;
 
    ---------------------------------------------------------------------------
