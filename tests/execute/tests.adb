0f7f93d77a63463f47a07f6d723896c1f951212a
Alexander Senier <senier@componolit.com>
2018-08-31 17:45:40 +0200

    Set exit status for tests

--- tests/execute/tests.adb
+++ tests/execute/tests.adb
@@ -12,10 +12,15 @@
 with SXML_Suite;
 with AUnit.Run;
 with AUnit.Reporter.Text;
+with Ada.Command_Line;
 
 procedure Tests is
-   procedure Run is new AUnit.Run.Test_Runner (SXML_Suite.Suite);
+   function Run is new AUnit.Run.Test_Runner_With_Status (SXML_Suite.Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
+   use AUnit;
+   S : Status;
 begin
-   Run (Reporter);
+   Reporter.Set_Use_ANSI_Colors (True);
+   S := Run (Reporter);
+   Ada.Command_Line.Set_Exit_Status ((if (S = Success) then 0 else 1));
 end Tests;
