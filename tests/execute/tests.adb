28a9aabe696868e110035729519d292645744d76
Alexander Senier <senier@componolit.com>
2018-09-07 10:39:00 +0200

    Indicate beginning of a test

--- tests/execute/tests.adb
+++ tests/execute/tests.adb
@@ -13,6 +13,7 @@ with SXML_Suite;
 with AUnit.Run;
 with AUnit.Reporter.Text;
 with Ada.Command_Line;
+with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Tests is
    function Run is new AUnit.Run.Test_Runner_With_Status (SXML_Suite.Suite);
@@ -20,6 +21,7 @@ procedure Tests is
    use AUnit;
    S : Status;
 begin
+   Put_Line ("Running SXML tests...");
    Reporter.Set_Use_ANSI_Colors (True);
    S := Run (Reporter);
    Ada.Command_Line.Set_Exit_Status ((if (S = Success) then 0 else 1));
