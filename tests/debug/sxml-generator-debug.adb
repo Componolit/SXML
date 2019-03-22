7e25fe9b7e3e9a6f034e5b99a739917c229c3e6a
Alexander Senier <senier@componolit.com>
2019-03-22 21:53:14 +0100

    Restructure build files

--- tests/debug/sxml-generator-debug.adb
+++ tests/debug/sxml-generator-debug.adb
@@ -1,6 +1,7 @@
 with SXML.Debug;
 
-package body SXML.Generator.Debug
+package body SXML.Generator.Debug with
+   SPARK_Mode => Off
 is
    procedure Dump (Context : Attributes_Type;
                    Short   : Boolean := False;
